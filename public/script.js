209103dee8908782d61cf4a8cc9a9143c4de31b2
aferguson9806 <aferguson9806@gmail.com>
2021-03-04 10:55:18 -0500

    added async function around fetch

--- public/script.js
+++ public/script.js
@@ -1,2 +1,7 @@
-const request = await fetch('/api');
-const data = await request.json();
+async function fetchPGData() {
+    const request = await fetch('/api');
+    const data = await request.json();
+};
+
+fetchPGData();
+
