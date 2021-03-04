805441c8e65c1b2d6629b6fd89545a4b471140dd
aferguson9806 <aferguson9806@gmail.com>
2021-03-04 11:56:25 -0500

    working post/gets from lecture

--- public/script.js
+++ public/script.js
@@ -1,7 +1,29 @@
-async function fetchPGData() {
-    const request = await fetch('/api');
+async function windowActions() {
+  const form = document.querySelector(".userform");
+  const search = document.querySelector("#textentry");
+
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    console.log("submit fired");
+    const request = await fetch("/api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ data: search.value }),
+    });
+
+
     const data = await request.json();
-};
+    console.table(data.data)
 
-fetchPGData();
+  });
 
+  
+
+  search.addEventListener("input", (event) => {
+    console.log("input", event.target.value);
+  });
+}
+
+window.onload = windowActions;
--- server.js
+++ server.js
@@ -22,18 +22,22 @@ app.use((req, res, next) => {
 
 
 app.route('/api')
-  .get((req, res) => {
+  .get(async (req, res) => {
     console.log('GET request detected');
-    res.send(`Lab 5 for ${process.env.NAME}`);
-  })
-  .post(async (req, res) => {
-    console.log('POST request detected');
-    console.log('Form data in res.body', req.body);
-
     const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
     const json = await data.json();
     console.log('data from fetch', json);
     res.json(json);
+  })
+  .post(async (req, res) => {
+    console.log('POST request detected');
+    console.log('Form data in res.body', req.body);
+    console.log('Now send something back to your client');
+
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const json = await data.json();
+
+    res.json({ data: json })
   });
 
 app.listen(port, () => {
